20d465207fddd006cf35f952f98b6c3bf24a2b54
kekule15 <onyekachiaugustus111@gmail.com>
2022-05-14 09:44:35 +0100

    project folder structure and dependencies

---
+++ newsapp/lib/styles/appColors.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color.fromARGB(255, 217, 5, 12);
+  static const goldBackground = Color(0xFFD5CBC1);
+  static const darkGold = Color(0xFF4C4742);
+  static const lightGold = Color(0xFFF5F2F0);
+  static const black = Color.fromARGB(255, 0, 0, 0);
+  static const black1 = Color(0xFF19090A);
+  static const keyboardBackground = Color.fromARGB(94, 210, 213, 219);
+  static const keyboardKeysShadow = Color(0xFF141414);
+  static const circlePinField = Color.fromARGB(87, 221, 226, 229);
+  static const textBlack = Color(0xFF121212);
+  static const termsTextColor = Color(0xFF393737);
+  static const scrollTrack = Color(0xFFDDDDDD);
+  static const white = Color(0xFFFFFFFF);
+  static const pureBlack = Color(0xFF000000);
+  static const green = Color(0xFF499F68);
+  static const lightGreen = Color(0xFFA4CFBE);
+  static const gray2 = Color(0xFFDDE2E5);
+  static const textGold = Color(0xFFCAB79F);
+  static const gray = Color.fromARGB(255, 170, 170, 170);
+  static const rareGold = Color(0xFFD5CBC1);
+  static const tabLabelColor = Color(0xFF545151);
+  static const unselectedTabIndicator = Color(0XFFACB5BD);
+  static const gray4 = Color(0xFF495057);
+  static const dropDownBorderColor = Color(0xFFC4C4C4);
+}
